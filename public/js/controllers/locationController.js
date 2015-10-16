8599624886422d0ea8fe5a1d595f0fa6e99da36a
EnteJulien <julien.ente@gmail.com>
2015-10-16 03:10:01 +0200

    carte et phrases sur le temps

--- public/js/controllers/locationController.js
+++ public/js/controllers/locationController.js
@@ -6,10 +6,10 @@ function locationController($scope, $routeParams, locationService){
 	locationService.recoverInfoLocation($scope.plage).then(function(response){
       //SUCCESS
       data = response.data;
+      adapter(data.main.temp);
       $scope.icone = data.weather[0].icon;
       $scope.temp = Math.round(data.main.temp);
       $scope.name = data.name;
-      console.log(response.data);
 
       var myLatLng = {lat: data.coord.lat, lng: data.coord.lon};
 
@@ -31,4 +31,28 @@ function locationController($scope, $routeParams, locationService){
       //ERROR
     })
 
+	var adapter = function(temp) {
+		console.log(temp);
+		var phrase = "";
+
+		if (temp <= -10) {
+			phrase = "On s'pèle le t'chu dins c'coin ! Pas d'bain, c'te jour !";
+		}
+		else if (temp > -10 && temp <= 0) {
+			phrase = "Fait nin chô, la mer ce sera pour e'd'min";
+		}
+		else if (temp > 0 && temp <= 8) {
+			phrase = "Fait bin tiède à'c'jour."
+		}
+		else if (temp > 0 && temp <= 15) {
+			phrase = "Au début elle est froide et après elle est bonne !"
+		}
+		else if (temp > 18 && temp <= 25) {
+			phrase = "Fait bin bon, on va aller à l'a' plage avec eun' canette !"
+		}
+		else {
+			phrase = "Ki fait kô, allez, on met le chu deul' flotte !"
+		}
+	$scope.adapter = phrase
+	};
 }

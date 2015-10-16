09cbafe12dc052afea5f078bac45304e851e8b3d
EnteJulien <julien.ente@gmail.com>
2015-10-16 04:16:21 +0200

    eul' vent

--- public/js/controllers/locationController.js
+++ public/js/controllers/locationController.js
@@ -5,11 +5,17 @@ function locationController($scope, $routeParams, locationService){
 
 	locationService.recoverInfoLocation($scope.plage).then(function(response){
       //SUCCESS
+
+
       data = response.data;
       adapter(data.main.temp);
+      souffler(data.wind.speed);
       $scope.icone = data.weather[0].icon;
       $scope.temp = Math.round(data.main.temp);
+      $scope.vent = data.wind.speed;
       $scope.name = data.name;
+
+      console.log(data);
 
       var myLatLng = {lat: data.coord.lat, lng: data.coord.lon};
 
@@ -54,5 +60,54 @@ function locationController($scope, $routeParams, locationService){
 			phrase = "Ki fait kô, allez, on met le chu deul' flotte !"
 		}
 	$scope.adapter = phrase
+	};
+
+	var souffler = function(vent) {
+		console.log(vent);
+		var beaufort = "";
+
+		if (vent <= 0.3) {
+			beaufort = "0 sur l'échelle eud' Beaufort, y'a pas un poil de moumoutte qui bouge.";
+		}
+		else if (vent > 0.3 && vent <= 1.5) {
+			beaufort = "1 sur l'échelle eud' Beaufort, l'herbe, el' boug'";
+		}
+		else if (vent > 1.5 && vent <= 3.3) {
+			beaufort = "2 sur l'échelle eud' Beaufort, ça fait d'l'air aux mollets."
+		}
+		else if (vent > 3.3 && vent <= 5.4) {
+			beaufort = "3 sur l'échelle eud' Beaufort, ça soulève eul' jupe eud' mémé"
+		}
+		else if (vent > 5.4 && vent <= 7.9) {
+			beaufort = "4 sur l'échelle eud' Beaufort, les capiods volent. "
+		}
+		else if (vent > 7.9 && vent <= 10.7) {
+			beaufort = "5 sur l'échelle eud' Beaufort";
+		}
+		else if (vent > 10.7 && vent <= 13.8) {
+			beaufort = "6 sur l'échelle eud' Beaufort"
+		}
+		else if (vent > 13.8 && vent <= 17.1) {
+			beaufort = "7 sur l'échelle eud' Beaufort"
+		}
+		else if (vent > 17.1 && vent <= 20.7) {
+			beaufort = "8 sur l'échelle eud' Beaufort"
+		}
+		else if (vent > 20.7 && vent <= 24.4) {
+			beaufort = "9 sur l'échelle eud' Beaufort"
+		}
+		else if (vent > 24.4 && vent <= 28.5) {
+			beaufort = "10 sur l'échelle eud' Beaufort"
+		}
+		else if (vent > 28.5 && vent <= 32.7) {
+			beaufort = "11 sur l'échelle eud' Beaufort, ça décorne eul'vac'"
+		}
+		else if (vent > 32.7 && vent <= 36.7) {
+			beaufort = "12 sur l'échelle eud' Beaufort, tout fout l'camp !"
+		}
+		else {
+			beaufort = "13 sur l'échelle eud' Beaufort. Y'a plus rin d'bout."
+		}
+	$scope.souffler = beaufort
 	};
 }

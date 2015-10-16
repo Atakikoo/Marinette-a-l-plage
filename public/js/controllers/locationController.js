9dfee928c11ad2e15f277ddd63215ad8781c1ab2
Atakikoo <atakikoo@gmail.com>
2015-10-16 05:30:51 +0200

    final

--- public/js/controllers/locationController.js
+++ public/js/controllers/locationController.js
@@ -48,16 +48,16 @@ function locationController($scope, $routeParams, locationService){
 			phrase = "Fait nin chô, la mer ce sera pour e'd'min";
 		}
 		else if (temp > 0 && temp <= 8) {
-			phrase = "Fait bin tiède à'c'jour."
+			phrase = "Fait bin tiède à'c'jour.";
 		}
 		else if (temp > 0 && temp <= 15) {
-			phrase = "Au début elle est froide et après elle est bonne !"
+			phrase = "Au début elle est froide et après elle est bonne !";
 		}
 		else if (temp > 18 && temp <= 25) {
-			phrase = "Fait bin bon, on va aller à l'a' plage avec eun' canette !"
+			phrase = "Fait bin bon, on va aller à l'a' plage avec eun' canette !";
 		}
 		else {
-			phrase = "Ki fait kô, allez, on met le chu deul' flotte !"
+			phrase = "Ki fait kô, allez, on met le chu deul' flotte !";
 		}
 	$scope.adapter = phrase
 	};
@@ -73,40 +73,40 @@ function locationController($scope, $routeParams, locationService){
 			beaufort = "1 sur l'échelle eud' Beaufort, l'herbe, el' boug' ";
 		}
 		else if (vent > 1.5 && vent <= 3.3) {
-			beaufort = "2 sur l'échelle eud' Beaufort, ça fait d'l'air aux mollets "
+			beaufort = "2 sur l'échelle eud' Beaufort, ça fait d'l'air aux mollets ";
 		}
 		else if (vent > 3.3 && vent <= 5.4) {
-			beaufort = "3 sur l'échelle eud' Beaufort, ça soulève eul' jupe eud' mémé "
+			beaufort = "3 sur l'échelle eud' Beaufort, ça soulève eul' jupe eud' mémé ";
 		}
 		else if (vent > 5.4 && vent <= 7.9) {
-			beaufort = "4 sur l'échelle eud' Beaufort, les capiots volent "
+			beaufort = "4 sur l'échelle eud' Beaufort, les capiots volent ";
 		}
 		else if (vent > 7.9 && vent <= 10.7) {
 			beaufort = "5 sur l'échelle eud' Beaufort, l'mair', i'en perd s'n'écharp' ";
 		}
 		else if (vent > 10.7 && vent <= 13.8) {
-			beaufort = "6 sur l'échelle eud' Beaufort, ti va m'fair' eul' plaisi d' rintrer., rent' vit' eut' miches "
+			beaufort = "6 sur l'échelle eud' Beaufort, ti va m'fair' eul' plaisi d' rintrer., rent' vit' eut' miches ";
 		}
 		else if (vent > 13.8 && vent <= 17.1) {
-			beaufort = "7 sur l'échelle eud' Beaufort, rent' vit' eut' miches "
+			beaufort = "7 sur l'échelle eud' Beaufort, rent' vit' eut' miches ";
 		}
 		else if (vent > 17.1 && vent <= 20.7) {
-			beaufort = "8 sur l'échelle eud' Beaufort, ça décoif' eul' chauv' "
+			beaufort = "8 sur l'échelle eud' Beaufort, ça décoif' eul' chauv' ";
 		}
 		else if (vent > 20.7 && vent <= 24.4) {
-			beaufort = "9 sur l'échelle eud' Beaufort, i a l'cloch' qui sonn' "
+			beaufort = "9 sur l'échelle eud' Beaufort, i a l'cloch' qui sonn' ";
 		}
 		else if (vent > 24.4 && vent <= 28.5) {
-			beaufort = "10 sur l'échelle eud' Beaufort, ça décorne eul'vac' "
+			beaufort = "10 sur l'échelle eud' Beaufort, ça décorne eul'vac' ";
 		}
 		else if (vent > 28.5 && vent <= 32.7) {
-			beaufort = "11 sur l'échelle eud' Beaufort, l'arb' del' gardin y s'barre "
+			beaufort = "11 sur l'échelle eud' Beaufort, l'arb' del' gardin y s'barre ";
 		}
 		else if (vent > 32.7 && vent <= 36.7) {
-			beaufort = "12 sur l'échelle eud' Beaufort, tout fout l'camp "
+			beaufort = "12 sur l'échelle eud' Beaufort, tout fout l'camp ";
 		}
 		else {
-			beaufort = "13 sur l'échelle eud' Beaufort. Y'a plus rin d'bout "
+			beaufort = "13 sur l'échelle eud' Beaufort. Y'a plus rin d'bout ";
 		}
 	$scope.souffler = beaufort
 	};
